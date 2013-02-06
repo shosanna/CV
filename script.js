a608858de33e713cc69a048db29868633554ef7e
Zuzana Dostalova <sanna23@icloud.com>
2013-02-06 21:02:15 +0100

    initial

---
+++ script.js
@@ -0,0 +1,8 @@
+// $(document).ready(function(){
+//   $(".header").mouseenter(function(){
+//     $(".header").stop().fadeTo("slow", 1);
+//   });
+//   $(".header").mouseleave(function(){
+//     $(".header").stop().fadeTo("slow", 0.5);
+//   });
+// });
